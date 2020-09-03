377debdf582de4d5f02f39bae0d4ff2f6f35583d
Tarun Nagpal <tarun.nagpal78@gmail.com>
2020-09-03 18:33:23 +0530

    Added the fizzbuzz calculation program

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser");
 // Body Parser MiddleWare
 app.use(bodyParser.json());
 
-// Start the APP at PORT 
+// Start the APP at PORT
 const port = 8000;
 app.listen(port, () => {
   console.log("listen port 8000");
@@ -24,6 +24,7 @@ app.post("/getFizzBuzzData", (req, res) => {
 
   if (req.body.number) {
     if (typeof req.body.number == "number") {
+      responseData = getFizzBuzz(req.body.number);
       showSuccess(res, { isError, responseData });
     } else {
       isError = true;
@@ -44,4 +45,21 @@ const showError = (res, errorResponse = {}) => {
 // Helper function for Success response
 const showSuccess = (res, successResponse = {}) => {
   res.status(200).json(successResponse);
+};
+
+
+/**
+ * Program to return the FizzBuzz data based on value
+ * @param {number} value 
+ */
+const getFizzBuzz = (value = 0) => {
+  var output = [];
+
+  for (var i = 1; i < value; i++) {
+    if (i % 15 == 0) output.push("FizzBuzz");
+    else if (i % 3 == 0) output.push("Fizz");
+    else if (i % 5 == 0) output.push("Buzz");
+    else output.push(i);
+  }
+  return output;
 };
